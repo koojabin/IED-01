440f94110b3dedde6a32bb1b194626b7d0763e2a
koojabin <66162016+koojabin@users.noreply.github.com>
2020-09-20 15:05:19 +0900

    Add files via upload

---
+++ 05_pratice_2/05_pratice_2.ino
@@ -0,0 +1,27 @@
+#define PIN_LED 7
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200); // Initialize serial port
+  while (!Serial) {
+    ; // wait for serial port to connect.
+  }
+  Serial.println("Hello World!");
+}
+
+void loop() {
+  digitalWrite(PIN_LED, 0);
+  delay(1000);
+  for(int i=0; i<5; i++)
+  {
+    digitalWrite(PIN_LED, 0);
+    delay(100);
+    digitalWrite(PIN_LED, 1);
+    delay(100);
+  }
+  while(1)
+  {
+
+  }
+}
